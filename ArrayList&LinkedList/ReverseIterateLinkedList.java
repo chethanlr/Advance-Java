99d36159416f286dbf8da3238c8c07c18a632822
Chethan L R <153184698+chethanlr@users.noreply.github.com>
2025-05-19 23:32:06 +0530

    `

--- ArrayList&LinkedList/ReverseIterateLinkedList.java
+++ ArrayList&LinkedList/ReverseIterateLinkedList.java
@@ -7,7 +7,7 @@ package ADV_JAVA;
 import java.util.LinkedList;
 import java.util.Iterator;
 
-public class LinkedList_1c_2 {
+public class ReverseIterateLinkedList {
 	    public static void main(String[] args) {
 	        // Create a LinkedList to store colors
 	        LinkedList<String> colors = new LinkedList<>();
